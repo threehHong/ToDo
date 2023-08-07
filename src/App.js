c7806aa485757e39100e6907a1caf3e5e5dabe69
threehHong <honghyohun@naver.com>
2023-08-07 19:08:49 +0900

    feat: 라우터 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import { useState } from 'react';
-import Form from './component/Form';
-import List from './component/List';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './page/Home';
+import Signup from './page/Signup';
 
 function App() {
-  const [todo, setTodo] = useState([]);
-  const [value, setValue] = useState("");
-  
-  let newTodo = {
-    id: Date.now(),
-    title: value,
-    completed: false,
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setTodo([...todo, newTodo])
-    setValue("");
-  }
-
   return (
-    <div style={{ width: '1200px', margin: '50px auto' }}>
-      <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
-      <List todo={todo} setTodo={setTodo} />
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<LoginForm />}> </Route> */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
