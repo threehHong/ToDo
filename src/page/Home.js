70e2b5b86f68bae8dc9681dda82fa5c8e25168b4
threehHong <honghyohun@naver.com>
2023-08-07 19:09:45 +0900

    feat: 라우터 추가로 인한 home 페이지 생성

---
+++ src/page/Home.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import Form from '../component/Form';
+import List from '../component/List';
+import axios from 'axios';
+
+/* const SERVER_URL = 'https://www.pre-onboarding-selection-task.shop/'; */
+
+export default function Home() {
+  const [todo, setTodo] = useState([]);
+  const [value, setValue] = useState("");
+  
+  let newTodo = {
+    id: Date.now(),
+    title: value,
+    completed: false,
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setTodo([...todo, newTodo])
+    setValue("");
+
+    /* await axios.post(SERVER_URL, {todo}).catch(error => console.log(error)) */
+  }
+
+  return (
+    <div style={{ width: '1200px', margin: '50px auto' }}>
+      <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
+      <List todo={todo} setTodo={setTodo} />
+    </div>
+  );
+}
