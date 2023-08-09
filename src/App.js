3e2238c16393fbb9a3e36df1091743ef1a75916f
threehHong <honghyohun@naver.com>
2023-08-09 18:21:59 +0900

    feat: 라우팅 설정 - 기본 경로에 Home 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Todo from './page/Todo';
 import Signup from './page/Signup';
 import Signin from './page/Signin';
+import Home from './page/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<LoginForm />}> </Route> */}
+        <Route path="/" element={<Home />}> </Route>
         <Route path="/signup" element={<Signup />} />
         <Route path="/Signin" element={<Signin />} />
         <Route path="/todo" element={<Todo />} />
