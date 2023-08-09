7edf6020461e3005c400e685cde941b3a8dadc51
threehHong <honghyohun@naver.com>
2023-08-09 18:25:34 +0900

    fix: styled 컴포넌트 재사용을 위한 export 추가

--- src/component/AuthForm.js
+++ src/component/AuthForm.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { styled } from 'styled-components'
 
-const Section =  styled.section`
+export const Section =  styled.section`
   height: 90vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `
 
-/* const SignupForm =  styled.form`
+export const SignupForm =  styled.form`
   border: 1px solid #aaa;
   width: 350px;
   height: 400px;
@@ -18,7 +18,7 @@ const Section =  styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-` */
+`
 
 export default function AuthForm({children}) {
   return (
