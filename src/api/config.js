e7a75ecc10ce60c951687c9967562b954936e347
threehHong <honghyohun@naver.com>
2023-08-10 18:06:38 +0900

    fix: interceptors 추가

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,21 @@
 import axios from "axios";
 
+console.log('config 출력 확인');
+
 export const axiosInstance = axios.create({
     baseURL: "https://www.pre-onboarding-selection-task.shop",
-    headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-    }
+})
+
+// 요청이 날아갈 때마다 실행되는 로직
+axiosInstance.interceptors.request.use((config) => {
+    
+    // header의 Authorization에 accessToken을 넣어주는 로직
+    const accessToken = localStorage.getItem("access_token");
+
+    config.headers.Authorization = 'Bearer ' + accessToken;
+
+    return config;
+}, 
+(error) => {
+    return Promise.reject(error);
 })
