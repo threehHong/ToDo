fc37bcea0514a9ac31dcbc425e9d16799791ee18
threehHong <honghyohun@naver.com>
2023-08-10 15:23:53 +0900

    refactor: header 이동

--- src/api/config.js
+++ src/api/config.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: "https://www.pre-onboarding-selection-task.shop",
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    }
 })
