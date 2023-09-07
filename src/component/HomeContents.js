1b4a81f1d577e8c8fb647858444eb9637c787015
threehHong <honghyohun@naver.com>
2023-09-07 12:00:11 +0900

    fix: styled-components 코드 하단으로 배치

--- src/component/HomeContents.js
+++ src/component/HomeContents.js
@@ -1,29 +1,6 @@
-import React, { useEffect } from 'react'
-import { styled } from 'styled-components'
-import { useNavigate } from 'react-router-dom';
-
-const Sign = styled.div`
-    display: flex;
-    gap: 30px;
-
-    & > span {
-        border-radius: 10px;
-        width: 100px;
-        line-height: 30px;
-        text-align: center;
-        color: #fff;
-        cursor: pointer;
-    }
-`
-const SignIn = styled.span`
-    border: 1px solid #16A1EF;
-    background: #16A1EF;
-`
-
-const SignUp = styled.span`
-    border: 1px solid #196B9A;
-    background: #196B9A;
-`
+import React, { useEffect } from "react";
+import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export default function HomeContents() {
   const accessToken = localStorage.getItem("access_token");
@@ -31,25 +8,43 @@ export default function HomeContents() {
 
   // 토큰이 있을 경우 todo 페이지로 이동 그렇지 않을 경우 유지.
   useEffect(() => {
-    if(accessToken) {
-      navigate('/todo');
+    if (accessToken) {
+      navigate("/todo");
     }
   }, []);
 
   const handleClick = (page) => {
-    page === 'signin'? navigate('/signin') : navigate('/signup');
-  }
-
+    page === "signin" ? navigate("/signin") : navigate("/signup");
+  };
 
   return (
-    <Sign> 
-        <SignIn onClick={() => handleClick('signin')}>
-            로그인
-        </SignIn>
+    <Sign>
+      <SignIn onClick={() => handleClick("signin")}>로그인</SignIn>
 
-        <SignUp onClick={() => handleClick('signup')}>
-            회원가입
-        </SignUp>
+      <SignUp onClick={() => handleClick("signup")}>회원가입</SignUp>
     </Sign>
-  )
+  );
 }
+
+const Sign = styled.div`
+  display: flex;
+  gap: 30px;
+
+  & > span {
+    border-radius: 10px;
+    width: 100px;
+    line-height: 30px;
+    text-align: center;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
+const SignIn = styled.span`
+  border: 1px solid #16a1ef;
+  background: #16a1ef;
+`;
+
+const SignUp = styled.span`
+  border: 1px solid #196b9a;
+  background: #196b9a;
+`;
